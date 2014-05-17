04cecc1695b01ac9a81c26a340fb7ab0d5628a82
Wolfram <WolframReinke@web.de>
2014-05-17 12:52:36 +0200

    Javadoc-Dokumentation

--- src/de/tungsten/tocs/engine/nodes/Player.java
+++ src/de/tungsten/tocs/engine/nodes/Player.java
@@ -5,15 +5,38 @@ import de.tungsten.tocs.config.ConfigurationType;
 import de.tungsten.tocs.engine.Team;
 import de.tungsten.tocs.engine.parsing.NodeLocator;
 
+/**
+ * Ein Spieler ist ein {@link Node Knoten} der einen Nickname, Geld, ein Team, ein
+ * Inventar und Hände hat.
+ * 
+ * @author Wolfram
+ *
+ */
 public class Player extends Node {
 
+	// Zum laden aus der Konfigurationsdatei
 	public	static final String CONFIG_PLAYER_CREDIT 	= "playerCredit";
 	private static final int 	DEFAULT_PLAYER_CREDIT	= 1000;
 	
-	private String nickname;
+	/**
+	 * Das Team dieses Spielers.
+	 */
 	private Team team;
+	
+	/**
+	 * Das aktuelle Guthaben dieses Spielers.
+	 */
 	private int credit;
 	
+	/**
+	 * Erstellt einen neuen Spieler unter Vergabe eines Nicknames und eines Teams. Als
+	 * Unterknoten werden automatisch das Inventar und die Hände des Spielers erstellt.
+	 * Der Startguthaben wird aus der Konfigurationsdatei gelesen.
+	 * 
+	 * @param nickname	Der Nickname des Spielers ist der einzige Name dieses Knotens
+	 * 					und damit gleichzetig der Identifier.
+	 * @param team		Das Team dieses Spielers.
+	 */
 	public Player( 
 			String nickname, 
 			Team team ) {
@@ -23,16 +46,15 @@ public class Player extends Node {
 				"This is " + nickname + "." );
 		
 		this.team = team;
-		this.nickname = nickname;
 		
-		// Load player's credit from config file
+		// Startguthaben aus Configuration ziehen
 		Configuration config = Configuration.getInstance();
 		credit = (int) config.getValue(
 				CONFIG_PLAYER_CREDIT, 
 				ConfigurationType.INTEGER, 
 				DEFAULT_PLAYER_CREDIT );
 		
-		// Create an inventory and a pair of hands (used to define to shoot with which weapon)
+		// Ein Inventar und ein Paar Hände erstellen.
 		this.children.add( new Node( 
 				this, 
 				new String[] { Node.NODE_INVENTORY, "inventory" }, 
@@ -43,22 +65,57 @@ public class Player extends Node {
 				"A pair of unkempt hands." ) );
 	}
 	
+	/**
+	 * Gibt das <code>Team</code> dieses Spielers zurück.
+	 * 
+	 * @return Das Team, von dem dieser Spieler ein Teil ist.
+	 */
 	public Team getTeam() {
 		return team;
 	}
 	
+	/**
+	 * Das aktuelle Guthaben des Spielers in Dollar.
+	 * 
+	 * @return	Das aktuelle Guthaben des Spielers.
+	 */
 	public int getMoney() {
 		return credit;
 	}
 	
+	/**
+	 * Der Nickname dieses Spielers. Diese Methode existiert nur zu Lesbarkeitsgründen
+	 * und ist gleichbedeutend mit
+	 * <pre>
+	 * getIdentifier()
+	 * </pre>
+	 * 
+	 * @return	Der Nickname/Identifier dieses Spielers.
+	 */
 	public String getNickname() {
-		return nickname;
+		return getIdentifier();
 	}
 	
+	/**
+	 * Das "Aussehen" (also die Beschreibung) des Spielers wird in dieser Methode
+	 * festgelegt.
+	 * 
+	 * @param skin	Die neue Beschreibung.
+	 */
 	public void setSkin( String skin ) {
 		description = skin;
 	}
 	
+	/**
+	 * Prüft, ob der gegebene Betrag vom Spieler aufgebracht werden kann. Wenn dies der
+	 * Fall ist, wird ihm der Betrag von Guthaben abgezogen und <code>true</code> wird
+	 * zurückgegeben. Anderfalls wird das Geld nicht verbraucht, und <code>false</code>
+	 * wird zurückgegeben.
+	 * 
+	 * @param amount	Der abzuziehende Betrag.
+	 * @return			<code>true</code>, wenn der Betrag von Guthaben des Spielers
+	 * 					abgezogen werden konnte, sonst <code>false</code>.
+	 */
 	public boolean consumeMoney( int amount ) {
 		if ( amount > credit ) return false;
 		else {
@@ -67,21 +124,38 @@ public class Player extends Node {
 		}
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * Spieler können nur Unterknoten von {@link Room Räumen} sein.
+	 */
 	@Override
 	public void move( Node target ) {
 		if ( target instanceof Room )
 			super.move( target );
 		
-		// TODO throw some kind of exception here
+		// TODO Irgendeine Exception werfen. Bisher wird aber tatsächlich eine 
+		// geworfen, also geht noch irgendwas schief
 	}
 	
+	/**
+	 * Gibt den Inventar-Knoten dieses Spielers zurück.
+	 * 
+	 * @return	Das Inventar des Spielers.
+	 */
 	public Node getInventory() {
 		
-		return NodeLocator.findSubNode( Node.NODE_INVENTORY, this, -1 );
+		return NodeLocator.findSubNode( Node.NODE_INVENTORY, this, 1 );
 	}
 	
+	/**
+	 * Gibt die Hände des Spielers zurück. Wenn der Spieler den Befehl "shoot" verwendet,
+	 * wird die Waffe abgefeuert, die sich in seiner Hand befindet.
+	 * 
+	 * @return	Die Hände des Spielers.
+	 */
 	public Node getHands() {
 		
-		return NodeLocator.findSubNode( Node.NODE_HANDS, this, -1 );
+		return NodeLocator.findSubNode( Node.NODE_HANDS, this, 1 );
 	}
 }
--- src/de/tungsten/tocs/engine/nodes/Room.java
+++ src/de/tungsten/tocs/engine/nodes/Room.java
@@ -5,17 +5,95 @@ import java.util.HashSet;
 import java.util.Map.Entry;
 import java.util.Set;
 
+/**
+ * Ein <code>Room</code> ist ein Knoten im Spiel, der geschlossen und 
+ * abgeschlossen (siehe {@link LockableNode} werden kann, und der Türen in
+ * alle Himmelsrichtungen, sowie nach oben und unten haben kann.
+ * <p>
+ * Die Türen sind nichts als Referenzen auf andere <code>Room</code>s. Alle
+ * erreichbaren Räume können mit {@link #getAdjacentRooms()} erfragt werden,
+ * einzelne Räume mit {@link #getAdjacentRoom(DoorDirection)}. Um herauszufinden,
+ * ob ein Raum in einer gegebenen Richtung liegt, kann 
+ * {@link #canAccess(DoorDirection)} verwendet werden.
+ * <p>
+ * Spieler in TOCS können nur innerhalb von <code>Room</code>-Knoten platziert
+ * werden, sie stellen somit die Umgebung dar, in der sich der Spieler bewegen
+ * kann. Dabei ist "Raum" nur ein allgemeines Wort, auch offenene Plätze können
+ * so modelliert werden.
+ * 
+ * @author Wolfram
+ *
+ */
 public class Room extends LockableNode {
 
+	/**
+	 * Speichert die Räume, die von diesem Raum erreichbar sind, indiziert nach
+	 * Richtung.
+	 */
 	private EnumMap<DoorDirection, Room> adjacentRooms = new EnumMap<DoorDirection, Room>( DoorDirection.class );
 	
-	public Room(Node parent, String[] names, String description, 
-			boolean closed, boolean locked) {
+	/**
+	 * Erstellt einen neuen Raum ohne Parent-Knoten. Dabei werden die Namen
+	 * und die Beschreibung (siehe {@link Node}) übergeben, sowie festgelegt,
+	 * ob der Knoten offen/geschlossen (siehe {@link OpenableNode}) und
+	 * abgeschlossen/aufgeschlossen (siehe {@link LockableNode}) ist.
+	 * 
+	 * @param names			Die Namen dieses Knotens, also die Zeichenketten,
+	 * 						mit denen sich Spieler auf diesen Knoten beziehen
+	 * 						können. Dieses Array muss mindestens einen Wert 
+	 * 						enthalten, sonst wird eine <code>NodeException</code>
+	 * 						geworfen.
+	 * @param description	Die Beschreibung dieses Knotens. Ist dieser Wert
+	 * 						<code>null</code> wird eine <code>NodeException</code>
+	 * 						geworfen.
+	 * @param closed		Gibt an, ob der Knoten geschlossen (<code>true</code>)
+	 * 						oder offen (<code>false</code>) ist.
+	 * @param locked		Gibt an, ob der Knoten abgeschlossen 
+	 * 						(<code>true</code>) oder aufgeschlossen
+	 * 						(<code>false</code>) ist.		
+	 */
+	public Room( String[] names, String description, 
+			boolean closed, boolean locked ) {
+		this( null, names, description, closed, locked );
+	}
+	
+	/**
+	 * Erstellt eine neue Instanz von <code>Room</code>. Dabei wird der
+	 * Parent-Knoten, die Namen und die Beschreibung (siehe {@link Node})
+	 * übergeben, sowie festgelegt, ob der Knonten offen oder geschlossen
+	 * (siehe {@link OpenableNode}) und abgeschlossen oder aufgeschlossen
+	 * (siehe {@link LockableNode}) ist.
+	 * 
+	 * @param parent		Der diesem Knoten übergeordnete Knoten.
+	 * @param names			Die Namen dieses Knotens, also die Zeichenketten,
+	 * 						mit denen sich Spieler auf diesen Knoten beziehen
+	 * 						können. Dieses Array muss mindestens einen Wert 
+	 * 						enthalten, sonst wird eine <code>NodeException</code>
+	 * 						geworfen.
+	 * @param description	Die Beschreibung dieses Knotens. Ist dieser Wert
+	 * 						<code>null</code> wird eine <code>NodeException</code>
+	 * 						geworfen.
+	 * @param closed		Gibt an, ob der Knoten geschlossen (<code>true</code>)
+	 * 						oder offen (<code>false</code>) ist.
+	 * @param locked		Gibt an, ob der Knoten abgeschlossen 
+	 * 						(<code>true</code>) oder aufgeschlossen
+	 * 						(<code>false</code>) ist.
+	 */
+	public Room( Node parent, String[] names, String description, 
+			boolean closed, boolean locked ) {
 		
-		super(parent, names, description, closed, locked);
+		super( parent, names, description, closed, locked );
 		
 	}
 	
+	/**
+	 * Gibt zurück, ob in der gegebenen Richtung ein Raum liegt. Es wird
+	 * auch <code>false</code> zurückgegeben, wenn dieser Raum, oder der
+	 * in der gegebenen Richtung liegende Raum abgeschlossen sein sollten.
+	 * 
+	 * @param direction	Die zu überprüfende Richtung.
+	 * @return			Ob in der gegebenen Richtung ein Raum erreichbar ist.
+	 */
 	public boolean canAccess( DoorDirection direction ) {
 		
 		if ( isLocked() ) return false;
@@ -26,10 +104,18 @@ public class Room extends LockableNode {
 	
 	}
 	
+	/**
+	 * Ermöglicht das iterieren über alle von diesem Raum aus erreichbaren
+	 * (siehe {@link #canAccess(DoorDirection)}) Räume.
+	 * 
+	 * @return	Ein <code>Iterable&lt;Room&gt;</code>, welches das iterieren
+	 * 			über alle erreichbaren Räume ermöglicht.
+	 */
 	public Iterable<Room> getAdjacentRooms() {
 
 		Set<Room> result = new HashSet<Room>();
 		for (Entry<DoorDirection, Room> door : adjacentRooms.entrySet()) {
+			// Wenn der Raum erreichbar ist, dann wird er hinzugefügt
 			if ( this.canAccess( door.getKey() ) )
 				result.add( door.getValue() );
 		}
@@ -37,12 +123,34 @@ public class Room extends LockableNode {
 		return result;
 	}
 	
+	/**
+	 * Gibt den Raum zurück, der in der gegebenen Richtung liegt,
+	 * unabhängig davon, ob der {@link #canAccess(DoorDirection) erreichbar}
+	 * ist.
+	 * 
+	 * @param direction		Die Richtung, in der der gewünschte Raum liegt.
+	 * @return				Der Raum der in der gewünschten Richtung lieft,
+	 * 						und <code>null</code> falls kein Raum in dieser
+	 * 						Richtung liegt.
+	 */
 	public Room getAdjacentRoom( DoorDirection direction ) {
 
 		return adjacentRooms.get( direction );
 	}
 
-	public void setAdjacentRoom( DoorDirection direction, Room room ) {
+	/**
+	 * Erstellt eine Verbindung von diesem Raum zu dem gegebenen Raum in
+	 * die gegebene Richtung. Der Aufruf von
+	 * {@link #getAdjacentRoom(DoorDirection)} mit derselbe 
+	 * <code>DoorDirection</code> gibt von nun an den gegebenen Raum zurück.
+	 * 
+	 * @param direction		Die Richtung, in die der Raum hinzugefügt werden
+	 * 						aoll.
+	 * @param room			Der hinzuzufügende Raum.
+	 * @return				Dieser Raum, zwecks chaining.
+	 */
+	public Room setAdjacentRoom( DoorDirection direction, Room room ) {
 		adjacentRooms.put( direction, room );
+		return this;
 	}
 }
